lihaohong6/mw-config

commit 8f0670324002a2e4e20a10247c4aa6ddb750cde8

Add $wgRestrictionLevels for tikipedia (#6250)
This PR adds a wiki-specific `$wgRestrictionLevels` configuration for
tikipedia.

---------

Co-authored-by: 1108-Kiju <1108-Kiju@users.noreply.github.com>

LocalSettings.php
@@ -5631,6 +5631,9 @@ $wgConf->settings += [
 			'editbureaucratprotected',
 			'editconsulprotected',
 		],
+		'+tikipediawiki' => [
+			'editextendedconfirmedprotected',
+		],
 		'+trwdeploymentwiki' => [
 			'bureaucrat',
 			'consul',
@@ -5762,6 +5765,9 @@ $wgConf->settings += [
 		'testwiki' => [
 			'editbureaucratprotected',
 			'editconsulprotected',
+		],
+		'tikipediawiki' => [
+			'editextendedconfirmedprotected',
 		],
 		'ultimatelevelbuilderwiki' => [
 			'editemailconfirmedprotected',

commit 569c0518488f5dfedd3c1c8f19d31f259593bc9e

Fix typo; 'tikipediawiki' -> 'tikiwiki' (#6349)
This PR fixes an incorrect database name introduced in #6250.

In the previous PR, I added a wiki-specific `$wgRestrictionLevels`
configuration, but I accidentally used `tikipedia` as the database name.
This PR corrects the database name to the proper one: `tikiwiki`.

Co-authored-by: 1108-Kiju <1108-Kiju@users.noreply.github.com>

LocalSettings.php
@@ -5810,7 +5810,7 @@ $wgConf->settings += [
 			'editbureaucratprotected',
 			'editconsulprotected',
 		],
-		'+tikipediawiki' => [
+		'+tikiwiki' => [
 			'editextendedconfirmedprotected',
 		],
 		'+trwdeploymentwiki' => [
@@ -5950,7 +5950,7 @@ $wgConf->settings += [
 			'editbureaucratprotected',
 			'editconsulprotected',
 		],
-		'tikipediawiki' => [
+		'tikiwiki' => [
 			'editextendedconfirmedprotected',
 		],
 		'ultimatelevelbuilderwiki' => [